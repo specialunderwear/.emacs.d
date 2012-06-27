80b40da45c83c511b4d07848a17cd7416ad38460
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-06-28 00:10:40 +0200

    Added puppet-mode

--- activate.el
+++ activate.el
@@ -1,5 +1,10 @@
 ;;activate packages
-(require 'smart-tab)                                                                                                                                                      
+(require 'smart-tab)
 (global-smart-tab-mode 1)      
+
+;; Puppet mode
+(autoload 'puppet-mode "puppet-mode" "Puppet Mode." t)
+(add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
+(add-to-list 'interpreter-mode-alist '("puppet" . puppet-mode))
 
 (provide 'activate)
