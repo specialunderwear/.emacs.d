718303fa5406e92456164ee42a3440a5c8333c86
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-07-02 01:22:02 +0200

    added css, nxml lua and yaml mode

---
+++ elpa/javascript-2.2.1/javascript-autoloads.el
@@ -0,0 +1,30 @@
+;;; javascript-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (javascript-mode) "javascript" "javascript.el"
+;;;;;;  (20464 56121))
+;;; Generated autoloads from javascript.el
+
+(autoload (quote javascript-mode) "javascript" "\
+Major mode for editing JavaScript source text.
+
+Key bindings:
+
+\\{javascript-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("javascript-pkg.el") (20464 56121 182392))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; javascript-autoloads.el ends here
